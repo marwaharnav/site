ad3bd1e3cce87256c8356dfcb1d0a2aa74ab909d
marwaharnav <marwaharnav0@gmail.com>
2025-03-21 17:59:13 +0000

    Deploying to gh-pages from @ marwaharnav/marwaharnav.github.io@cf078506a5671dfd981edceb6a5fafeb6849d05b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "a collection of projects I have worked on",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
